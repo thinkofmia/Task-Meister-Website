696671ffeac4ec6dd5e9747f4b15800743ae1814
FremontTeng <fremontteng@gmail.com>
2020-05-12 11:51:00 +0800

    Added new plugin, cleaned up default plugins.

---
+++ plugins/tm_recommender/tm_recommender.php
@@ -0,0 +1,29 @@
+<?php
+// no direct access
+defined( '_JEXEC' ) or die;
+
+class plgTaskMeisterTM_recommender extends JPlugin
+{
+	/**
+	 * Load the language file on instantiation. Note this is only available in Joomla 3.1 and higher.
+	 * If you want to support 3.0 series you must override the constructor
+	 *
+	 * @var    boolean
+	 * @since  3.1
+	 */
+	protected $autoloadLanguage = true;
+
+	/**
+	 * Plugin method with the same name as the event will be called automatically.
+	 */
+	 function calculateMetrics($number)
+	 {
+		/*
+		 * Plugin code goes here.
+		 * You can access database and application objects and parameters via $this->db,
+		 * $this->app and $this->params respectively
+		 */
+        return "Wonderful ".$number;
+	}
+}
+?>
