ce5b9bcd045b5386fd201442acd0692390bd11e3
FremontTeng <fremontteng@gmail.com>
2020-05-13 11:51:36 +0800

    Added auto update user table func

--- plugins/tm_recommender/tm_recommender.php
+++ plugins/tm_recommender/tm_recommender.php
@@ -28,6 +28,80 @@ class plgTaskMeisterTM_recommender extends JPlugin
         return "Wonderful ".($number*2);
     }
 
+    function createList($array_str){
+        $array = json_decode($array_str,true);
+        $list = array_values($array);
+        $list_str = json_encode($list);
+        if (!isset($list_str)) $list_str = '[]';
+        return $list_str;
+    }
+
+    function fixUserStats(){
+        $db = Factory::getDbo();
+        $me = Factory::getUser();
+        //Get user bank
+        $query2 = $db->getQuery(true);
+        $query2->select($db->quoteName(array('id','name','email')))
+            ->from($db->quoteName('#__users'));
+        $db->setQuery($query2);
+        $results_bank = $db->loadAssocList();
+
+        //For Existing Accounts
+        $query = $db->getQuery(true);
+        $query->select($db->quoteName(array('a.*')))
+            ->from($db->quoteName('#__customtables_table_userstats','a'));
+        $db->setQuery($query);
+        $results_ext = $db->loadAssocList();
+
+        $debug = array();
+        $ext_users =array();
+        foreach ($results_ext as $row2) { 
+            array_push($ext_users, $row2['es_userid']);
+        }
+        //Add in new users
+        foreach ($results_bank as $row){
+            //Update user class
+            $userInfo = new stdClass();
+            $userInfo->es_userid = $row['id'];
+            $userInfo->es_name = $row['name'];
+            $userInfo->es_email = $row['email'];
+            if (in_array($row['id'], $ext_users)){
+                // Update the object into the article profile table.
+                $result = JFactory::getDbo()->updateObject('#__customtables_table_userstats', $userInfo, 'es_userid');
+            }
+            else{
+                $result = JFactory::getDbo()->insertObject('#__customtables_table_userstats', $userInfo, 'es_userid');
+            }
+        }   
+
+            //Update display
+            foreach ($results_ext as $row) { 
+                //For deployment list
+                if (isset($row['es_pagedeployed'])) $deployedList = $this->createList($row['es_pagedeployed']);
+                else $deployedList = "[]";
+                //For Liked pages
+                if (isset($row['es_pageliked'])) $likedList = $this->createList($row['es_pageliked']);
+                else $likedList = "[]";
+                //For Disliked pages
+                if (isset($row['es_pagedisliked'])) $dislikedList = $this->createList($row['es_pagedisliked']);
+                else $dislikedList = "[]";
+                //For user preference
+                if (isset($row['es_userpreference'])) $preferenceList = $this->createList($row['es_userpreference']);
+                else $preferenceList = "[]";
+                //Create user class
+                $userInfo = new stdClass();
+                $userInfo->es_userid = $row['es_userid'];
+                $userInfo->es_pageliked =  $likedList;
+                $userInfo->es_pagedisliked =  $dislikedList;
+                $userInfo->es_pagedeployed =  $deployedList;
+                $userInfo->es_userpreference =  $preferenceList;
+        
+                // Update the object into the article profile table.
+                $result = JFactory::getDbo()->updateObject('#__customtables_table_userstats', $userInfo, 'es_userid');
+        }
+        return true;//json_encode($debug);
+    }
+
     function updateArticleInfo(){//WIP
         return false;
     }
