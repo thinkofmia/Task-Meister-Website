34015907573aa46a1ee558526fedcd68ee331fee
FremontTeng <fremontteng@gmail.com>
2020-05-13 14:07:00 +0800

    Added documentation to plugin

--- plugins/tm_recommender/tm_recommender.php
+++ plugins/tm_recommender/tm_recommender.php
@@ -18,6 +18,7 @@ class plgTaskMeisterTM_recommender extends JPlugin
 	/**
 	 * Plugin method with the same name as the event will be called automatically.
 	 */
+    //Test Function
 	 function calculateMetrics($number)
 	 {
 		/*
@@ -27,38 +28,49 @@ class plgTaskMeisterTM_recommender extends JPlugin
 		 */
         return "Wonderful ".($number*2);
     }
-
-    function createList($array_str){
-        $array = json_decode($array_str,true);
-        $list = array_values($array);
-        $list_str = json_encode($list);
-        if (!isset($list_str)) $list_str = '[]';
-        return $list_str;
+    /* Function: Create List
+    This function creates an unordered array from an existing one.
+    Can be used anywhere
+     */
+    function createList($array_str){//Parameters used must be an array string
+        $array = json_decode($array_str,true);//Decodes string into array
+        $list = array_values($array);//Get the values of the array and store into an array (Removes the presence of keys)
+        $list_str = json_encode($list);//Stores into a string
+        if (!isset($list_str)) $list_str = '[]';//If null, set as empty instead
+        return $list_str;//Return new unordered array
     }
 
+    function fixArticleStats(){
+        return false;
+    }
+    
+    /* Function: Fix User Statistics
+    This function automatically updates/refreshes the user statistics.
+     It should be used when adding a new user/updating user profiles
+     */
     function fixUserStats(){
-        $db = Factory::getDbo();
-        $me = Factory::getUser();
-        //Get user bank
+        $db = Factory::getDbo();//Gets database
+        $me = Factory::getUser();//Gets user 
+        //Get user bank from database query
         $query2 = $db->getQuery(true);
         $query2->select($db->quoteName(array('id','name','email')))
             ->from($db->quoteName('#__users'));
         $db->setQuery($query2);
         $results_bank = $db->loadAssocList();
 
-        //For Existing Accounts
+        //Get external user table (custom table)
         $query = $db->getQuery(true);
         $query->select($db->quoteName(array('a.*')))
             ->from($db->quoteName('#__customtables_table_userstats','a'));
         $db->setQuery($query);
         $results_ext = $db->loadAssocList();
 
-        $debug = array();
+        //Store the current users in external table into an array
         $ext_users =array();
         foreach ($results_ext as $row2) { 
             array_push($ext_users, $row2['es_userid']);
         }
-        //Add in new users
+        //Add in new users if any
         foreach ($results_bank as $row){
             //Update user class
             $userInfo = new stdClass();
@@ -66,15 +78,15 @@ class plgTaskMeisterTM_recommender extends JPlugin
             $userInfo->es_name = $row['name'];
             $userInfo->es_email = $row['email'];
             if (in_array($row['id'], $ext_users)){
-                // Update the object into the article profile table.
+                // Update user info if exists.
                 $result = JFactory::getDbo()->updateObject('#__customtables_table_userstats', $userInfo, 'es_userid');
             }
-            else{
+            else{//Insert user info if doesn't exists
                 $result = JFactory::getDbo()->insertObject('#__customtables_table_userstats', $userInfo, 'es_userid');
             }
         }   
 
-            //Update display
+        //Update more statistics elements
             foreach ($results_ext as $row) { 
                 //For deployment list
                 if (isset($row['es_pagedeployed'])) $deployedList = $this->createList($row['es_pagedeployed']);
@@ -95,27 +107,31 @@ class plgTaskMeisterTM_recommender extends JPlugin
                 $userInfo->es_pagedisliked =  $dislikedList;
                 $userInfo->es_pagedeployed =  $deployedList;
                 $userInfo->es_userpreference =  $preferenceList;
-        
-                // Update the object into the article profile table.
+                // Update the object into the custom user table.
                 $result = JFactory::getDbo()->updateObject('#__customtables_table_userstats', $userInfo, 'es_userid');
         }
-        return true;//json_encode($debug);
+        return true;//Can return anything. Used true to see if succeeded.
     }
-
-    function updateArticleInfo(){//WIP
-        return false;
-    }
-
-    function countArticleLikes($list){
+    /* Function: Count Article Likes
+    This function calculates the total likes and dislikes of an article.
+    Can be used anywhere.
+     */
+    function countArticleLikes($list){//Parameters: Requires Article's user choice list
+        //Initialize variables
         $NoLikes = 0;
         $NoDislikes = 0;
+        //For loop counting
         foreach ($list as $row){
             if ($row == "Liked") $NoLikes +=1;
             else if ($row == "Disliked") $NoDislikes +=1;
         }
+        //Return an array of numbers. To access it, its something like $result[0][0] or $result[0][1]
         return array($NoLikes,$NoDislikes);
     }
-    
+    /* Function: Welcome Text
+    Welcome message of the plugin.
+    Can be used anywhere.
+     */
     function welcomeText(){
         $db = Factory::getDbo();
         $me = Factory::getUser();
