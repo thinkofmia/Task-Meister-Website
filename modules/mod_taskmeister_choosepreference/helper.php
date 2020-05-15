1dd31579e96b6532ba4dc3007883d34ec6e97cb2
FremontTeng <fremontteng@gmail.com>
2020-05-15 15:02:50 +0800

    Added base file for choose preference module

---
+++ modules/mod_taskmeister_choosepreference/helper.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Helper class for Hello World! module
+ * 
+ * @package    Joomla.Tutorials
+ * @subpackage Modules
+ * @link http://docs.joomla.org/J3.x:Creating_a_simple_module/Developing_a_Basic_Module
+ * @license        GNU/GPL, see LICENSE.php
+ * mod_helloworld is free software. This version may have been modified pursuant
+ * to the GNU General Public License, and as distributed it includes or
+ * is derivative of works licensed under the GNU General Public License or
+ * other free or open source software licenses.
+ */
+class ModChoosePreferenceHelper
+{
+    /**
+     * Retrieves the hello message
+     *
+     * @param   array  $params An object containing the module parameters
+     *
+     * @access public
+     */    
+    public static function getText($params)
+    {
+        return $params->get('customtext');
+    }
+    public static function getHeader($params)
+    {
+        return $params->get('customheader');
+    }
+}
