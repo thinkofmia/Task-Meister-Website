ede78249dd9bd556607aebdf7769a7f07af6b8e6
FremontTeng <fremontteng@gmail.com>
2020-05-18 16:09:22 +0800

    Added preference list

--- modules/mod_taskmeister_choosepreference/tmpl/default.php
+++ modules/mod_taskmeister_choosepreference/tmpl/default.php
@@ -4,6 +4,13 @@ defined('_JEXEC') or die;
 //Displays module output
 ?>
 
+<script type="text/javascript">
+    userPreferenceList_JS = [];
+    <?php foreach ($tagList as $key => $value) : ?>
+        userPreferenceList_JS.push("<?php echo $key; ?>");
+    <?php endforeach; ?>
+</script>
+
 <div class="customtext preferenceOptions">
     <?php if ($displayHeader) : ?>
         <h3><?php echo $displayHeader; ?></h3>
@@ -17,7 +24,7 @@ defined('_JEXEC') or die;
     <?php foreach ($tagList as $key => $value) : ?>
         <div class="preferenceBox" id="<?php echo $key;?>">
             <img src="/taskmeisterx/templates/taskmeistertemplate-userpage/images/accountIcon.jpg" width="100%" height="100%" />
-            <p><?php echo $key; ?></p>
+            <p><?php echo $key; ?>: <?php echo $value; ?> uses</p>
         </div>
     <?php endforeach; ?>
 </div>
