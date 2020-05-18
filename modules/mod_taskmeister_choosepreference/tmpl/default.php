a0132ef7955ff38cf6f36a7bf2ebf28a68d563eb
FremontTeng <fremontteng@gmail.com>
2020-05-18 15:58:26 +0800

    Remove bugs to display and set id = key

--- modules/mod_taskmeister_choosepreference/tmpl/default.php
+++ modules/mod_taskmeister_choosepreference/tmpl/default.php
@@ -4,7 +4,7 @@ defined('_JEXEC') or die;
 //Displays module output
 ?>
 
-<div class="customtext">
+<div class="customtext preferenceOptions">
     <?php if ($displayHeader) : ?>
         <h3><?php echo $displayHeader; ?></h3>
     <?php endif; ?>
@@ -15,9 +15,9 @@ defined('_JEXEC') or die;
 
 <div class="preferenceList">
     <?php foreach ($tagList as $key => $value) : ?>
-        <div class="preferenceBox">
+        <div class="preferenceBox" id="<?php echo $key;?>">
             <img src="/taskmeisterx/templates/taskmeistertemplate-userpage/images/accountIcon.jpg" width="100%" height="100%" />
-            <p><?php echo json_encode($key); ?></p>
+            <p><?php echo $key; ?></p>
         </div>
     <?php endforeach; ?>
 </div>
