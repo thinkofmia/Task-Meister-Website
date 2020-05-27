80d82a6c4552f07396d1bbd230c18ce0579a81fd
FremontTeng <fremontteng@gmail.com>
2020-05-27 11:29:22 +0800

    Added features inside featured article.

--- modules/mod_taskmeister_featuredarticle/tmpl/default.php
+++ modules/mod_taskmeister_featuredarticle/tmpl/default.php
@@ -3,23 +3,34 @@
 defined('_JEXEC') or die; 
 //Displays module output
 ?>
-
-<div class="featureArticle">
-    <!--If exists video, play video. Else show image of article.-->
-    <?php if ($videoLink) : ?>
-        <iframe width="300" height="300" src="<?php echo $videoLink; ?>">
-        </iframe>
-    <?php else : ?>
-        <img width="300" height="300" src="<?php echo $articleImage; ?>"></img>
-    <?php endif; ?>
-    <!--Displays Custom Header if exists, else use article title-->
-    <?php if ($displayHeader) : ?>
-        <h3><?php echo $displayHeader; ?></h3>
-    <?php else : ?>
-        <h3><?php echo $articleTitle; ?></h3>
-    <?php endif; ?>
-    <!--Displays Text if exists-->
-    <?php if ($displayText) : ?>
-        <?php echo $displayText; ?>
-    <?php endif; ?>
+<div class="wrapper">
+    <div class="featuredArticle">
+        <!--If exists video, play video. Else show image of article.-->
+        <?php if ($videoLink) : ?>
+            <iframe width="300" height="300" src="<?php echo $videoLink; ?>">
+            </iframe>
+        <?php else : ?>
+            <img width="300" height="300" src="<?php echo $articleImage; ?>"></img>
+        <?php endif; ?>
+        <div class="featuredArticleText">
+            <!--Displays Custom Header if exists, else use article title-->
+            <?php if ($displayHeader) : ?>
+                <h3><?php echo $displayHeader; ?></h3>
+            <?php else : ?>
+                <h3><?php echo $articleTitle; ?></h3>
+            <?php endif; ?>
+            <!--Displays Text if exists-->
+            <?php if ($displayText) : ?>
+                <p><?php echo $displayText; ?></p>
+            <?php endif; ?>
+            <!--Displays who has liked it-->
+            <p><b>Users who like this: </b><?php echo "Work In Progress"; ?></p>
+            <!--Displays those in your school that has deployed it-->
+            <p><b>Users who deploy this: </b><?php echo "Work In Progress"; ?></p>
+            <!--Displays play button-->
+            <p>
+                <a>▶ Play</a>
+            </p>
+        </div>
+    </div>
 </div>
