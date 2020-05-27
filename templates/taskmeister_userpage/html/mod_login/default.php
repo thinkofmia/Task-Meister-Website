a0e2aeae0245986b05678f73c60a1ddd8d32f1ca
FremontTeng <fremontteng@gmail.com>
2020-05-27 16:45:14 +0800

    Added css for login override

--- templates/taskmeister_userpage/html/mod_login/default.php
+++ templates/taskmeister_userpage/html/mod_login/default.php
@@ -28,7 +28,6 @@ JHtml::_('bootstrap.tooltip');
 					<div class="input-prepend">
 						<span class="add-on">
 							<span class="icon-user hasTooltip" title="<?php echo JText::_('MOD_LOGIN_VALUE_USERNAME'); ?>"></span>
-							<label for="modlgn-username" class="element-invisible"><?php echo JText::_('MOD_LOGIN_VALUE_USERNAME'); ?></label>
 						</span>
 						<input id="modlgn-username" type="text" name="username" class="input-small" tabindex="0" size="18" placeholder="<?php echo JText::_('MOD_LOGIN_VALUE_USERNAME'); ?>" />
 					</div>
@@ -45,8 +44,6 @@ JHtml::_('bootstrap.tooltip');
 						<span class="add-on">
 							<span class="icon-lock hasTooltip" title="<?php echo JText::_('JGLOBAL_PASSWORD'); ?>">
 							</span>
-								<label for="modlgn-passwd" class="element-invisible"><?php echo JText::_('JGLOBAL_PASSWORD'); ?>
-							</label>
 						</span>
 						<input id="modlgn-passwd" type="password" name="password" class="input-small" tabindex="0" size="18" placeholder="<?php echo JText::_('JGLOBAL_PASSWORD'); ?>" />
 					</div>
@@ -56,43 +53,16 @@ JHtml::_('bootstrap.tooltip');
 				<?php endif; ?>
 			</div>
 		</div>
-		<?php if (count($twofactormethods) > 1) : ?>
-		<div id="form-login-secretkey" class="control-group">
-			<div class="controls">
-				<?php if (!$params->get('usetext', 0)) : ?>
-					<div class="input-prepend input-append">
-						<span class="add-on">
-							<span class="icon-star hasTooltip" title="<?php echo JText::_('JGLOBAL_SECRETKEY'); ?>">
-							</span>
-								<label for="modlgn-secretkey" class="element-invisible"><?php echo JText::_('JGLOBAL_SECRETKEY'); ?>
-							</label>
-						</span>
-						<input id="modlgn-secretkey" autocomplete="one-time-code" type="text" name="secretkey" class="input-small" tabindex="0" size="18" placeholder="<?php echo JText::_('JGLOBAL_SECRETKEY'); ?>" />
-						<span class="btn width-auto hasTooltip" title="<?php echo JText::_('JGLOBAL_SECRETKEY_HELP'); ?>">
-							<span class="icon-help"></span>
-						</span>
-				</div>
-				<?php else : ?>
-					<label for="modlgn-secretkey"><?php echo JText::_('JGLOBAL_SECRETKEY'); ?></label>
-					<input id="modlgn-secretkey" autocomplete="one-time-code" type="text" name="secretkey" class="input-small" tabindex="0" size="18" placeholder="<?php echo JText::_('JGLOBAL_SECRETKEY'); ?>" />
-					<span class="btn width-auto hasTooltip" title="<?php echo JText::_('JGLOBAL_SECRETKEY_HELP'); ?>">
-						<span class="icon-help"></span>
-					</span>
-				<?php endif; ?>
-
-			</div>
-		</div>
-		<?php endif; ?>
-		<?php if (JPluginHelper::isEnabled('system', 'remember')) : ?>
-		<div id="form-login-remember" class="control-group checkbox">
-			<label for="modlgn-remember" class="control-label"><?php echo JText::_('MOD_LOGIN_REMEMBER_ME'); ?></label> <input id="modlgn-remember" type="checkbox" name="remember" class="inputbox" value="yes"/>
-		</div>
-		<?php endif; ?>
 		<div id="form-login-submit" class="control-group">
 			<div class="controls">
 				<button type="submit" tabindex="0" name="Submit" class="btn btn-primary login-button"><?php echo JText::_('JLOGIN'); ?></button>
 			</div>
 		</div>
+		<?php if (JPluginHelper::isEnabled('system', 'remember')) : ?>
+		<div id="form-login-remember" class="control-group checkbox">
+			<label for="modlgn-remember" class="control-label"><?php echo JText::_('MOD_LOGIN_REMEMBER_ME'); ?></label> <input id="modlgn-remember" type="checkbox" name="remember" class="inputbox" value="yes"/>
+		</div>
+		<?php endif; ?>
 		<?php
 			$usersConfig = JComponentHelper::getParams('com_users'); ?>
 			<ul class="unstyled">
@@ -102,14 +72,6 @@ JHtml::_('bootstrap.tooltip');
 					<?php echo JText::_('MOD_LOGIN_REGISTER'); ?> <span class="icon-arrow-right"></span></a>
 				</li>
 			<?php endif; ?>
-				<li>
-					<a href="<?php echo JRoute::_('index.php?option=com_users&view=remind'); ?>">
-					<?php echo JText::_('MOD_LOGIN_FORGOT_YOUR_USERNAME'); ?></a>
-				</li>
-				<li>
-					<a href="<?php echo JRoute::_('index.php?option=com_users&view=reset'); ?>">
-					<?php echo JText::_('MOD_LOGIN_FORGOT_YOUR_PASSWORD'); ?></a>
-				</li>
 			</ul>
 		<input type="hidden" name="option" value="com_users" />
 		<input type="hidden" name="task" value="user.login" />
