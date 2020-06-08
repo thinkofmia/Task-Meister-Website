102bd8a044472c7f9b52dd0287f466407273b9f0
Ectotater <36845332+Ectotater@users.noreply.github.com>
2020-06-08 18:13:55 +0800

    Removed whitespace

--- com_helloworld/site/helloworld.php
+++ com_helloworld/site/helloworld.php
@@ -7,16 +7,16 @@
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
- // No direct access to this file
- defined('_JEXEC') or die('Restricted access');
+// No direct access to this file
+defined('_JEXEC') or die('Restricted access');
 
- // Get an instance of the controller prefixed by HelloWorld; HelloWorldController
- // Joomla automatically looks for the declaration of HelloWorldController in './controller.php'
- $controller = JControllerLegacy::getInstance('HelloWorld');
+// Get an instance of the controller prefixed by HelloWorld; HelloWorldController
+// Joomla automatically looks for the declaration of HelloWorldController in './controller.php'
+$controller = JControllerLegacy::getInstance('HelloWorld');
 
- // Perfrom the Request task
- $input = JFactory::getApplication()->input;
- $controller->execute($input->getCmd('task'));
+// Perfrom the Request task
+$input = JFactory::getApplication()->input;
+$controller->execute($input->getCmd('task'));
 
- // Redirect if set by the controller
- $controller->redirect();
+// Redirect if set by the controller
+$controller->redirect();
