8656abc03ee9f3b7120bd9bc14916466993242cd
FremontTeng <fremontteng@gmail.com>
2020-06-09 16:39:28 +0800

    Rearranged the php code to diff files

--- modules/mod_taskmeister_yourclassstats/mod_taskmeister_yourclassstats.php
+++ modules/mod_taskmeister_yourclassstats/mod_taskmeister_yourclassstats.php
@@ -37,7 +37,33 @@ if ($userID!=0){//if User id isnt a guest
     $db->setQuery($query);
     $results = $db->loadAssocList();//Save results as $results2
 
-    if ($results){
+    if ($results){//If teacher data exists
+        foreach ($results as $row){//Extract teacher data
+            $teacher = JFactory::getUser($row['es_teacherid']);//Get Teacher Profile
+            $teacherName = $teacher->name;//Get Teacher Name
+            //Get the student list
+            $studentsList = json_decode($row['es_students']);
+        }
+        //Create Preferences Score Array
+        $fullPreferencesScore = array();
+        //Loop base on students list
+        foreach ($studentsList as $row){
+            $query = $db->getQuery(true);
+            $query->select($db->quoteName(array('es_userid','es_userpreference')))//Get user id, user preference
+                ->from($db->quoteName('#__customtables_table_userstats'))//From our external user stats table
+                ->where($db->quoteName('es_userid') . ' = ' . $row);//Where it is the current user's userid
+            $db->setQuery($query);
+            $results2 = $db->loadAssocList();//Save results as $results2
+            foreach ($results2 as $row2){
+                $studentPreferences = json_decode($row2['es_userpreference']);
+                foreach ($studentPreferences as $key => $value){
+                    if (isset($fullPreferencesScore[$key])) $fullPreferencesScore[$key] += $value;
+                    else $fullPreferencesScore[$key] = $value;
+                }
+            }
+        }
+        //Sort Preference Score by highest first
+        arsort($fullPreferencesScore);
         require JModuleHelper::getLayoutPath('mod_taskmeister_yourclassstats');
     }
     else{
--- modules/mod_taskmeister_yourclassstats/tmpl/default.php
+++ modules/mod_taskmeister_yourclassstats/tmpl/default.php
@@ -12,14 +12,6 @@ defined('_JEXEC') or die;
         <th>Analysis</th>
     </tr>
 <!--Show the external teacher stats-->
-<?php foreach ($results as $row): ?><!--For loop for each item in $results-->
-    <!--Set Teacher-->
-    <?php 
-    $teacher = JFactory::getUser($row['es_teacherid']);
-    $teacherName = $teacher->name;
-    //Set the student list for each teacher
-    $studentsList = json_decode($row['es_students']);
-    ?>
     <!--Print out the data-->
     <tr>
         <td><?php echo $teacherName; ?></td>
@@ -39,26 +31,8 @@ defined('_JEXEC') or die;
         <?php endif; ?>
         </td>
         <td>
-        <?php if ($studentsList) : ?>
-            <?php $fullPreferencesScore = array(); ?>
+        <?php if ($fullPreferencesScore) : ?>
             <!--For each student in the list, get preferences-->    
-            <?php foreach ($studentsList as $row){
-                $query = $db->getQuery(true);
-                $query->select($db->quoteName(array('es_userid','es_userpreference')))//Get user id, user preference
-                    ->from($db->quoteName('#__customtables_table_userstats'))//From our external user stats table
-                    ->where($db->quoteName('es_userid') . ' = ' . $row);//Where it is the current user's userid
-                $db->setQuery($query);
-                $results2 = $db->loadAssocList();//Save results as $results2
-                foreach ($results2 as $row2){
-                    $studentPreferences = json_decode($row2['es_userpreference']);
-                    foreach ($studentPreferences as $key => $value){
-                        if (isset($fullPreferencesScore[$key])) $fullPreferencesScore[$key] += $value;
-                        else $fullPreferencesScore[$key] = $value;
-                    }
-                }
-            }
-            arsort($fullPreferencesScore); 
-            ?>
             <ul>
             <?php foreach ($fullPreferencesScore as $key => $value) : ?>
                 <?php if (intval($value)>0) : ?>
@@ -71,5 +45,4 @@ defined('_JEXEC') or die;
         <?php endif ?>
         </td>
     </tr>
-    <?php endforeach ?>
 </table>
