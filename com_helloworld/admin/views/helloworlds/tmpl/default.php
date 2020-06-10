597e66d9aae250e01edf754d539e21a408c58df5
Ectotater <36845332+Ectotater@users.noreply.github.com>
2020-06-10 20:00:19 +0800

    Update default.php
    Added links for editing
    Fixed form functionality

--- com_helloworld/admin/views/helloworlds/tmpl/default.php
+++ com_helloworld/admin/views/helloworlds/tmpl/default.php
@@ -38,8 +38,9 @@ defined('_JEXEC') or die('Restricted access');
         </tfoot>
         <tbody>
             <?php if(!empty($this->items)) : ?>
-                <?php foreach($this->items as $i => $row) : ?>
-
+                <?php foreach($this->items as $i => $row) :
+                    $link = JRoute::_('index.php?option=com_helloworld&task=helloworld.edit&id=' . $row->id);    
+                ?>
                     <tr>
                         <td>
                             <?php echo $this->pagination->getRowOffset($i); ?>
@@ -48,6 +49,7 @@ defined('_JEXEC') or die('Restricted access');
                             <?php echo JHtml::_('grid.id', $i, $row->id); ?>
                         </td>
                         <td>
+                            <a href="<?php echo $link; ?>" title="<?php echo JText::_('COM_HELLOWORLD_EDIT_HELLOWORLD'); ?>">
                             <?php echo $row->greeting; ?>
                         </td>
                         <td align="center">
@@ -61,5 +63,7 @@ defined('_JEXEC') or die('Restricted access');
             <?php endif; ?>
         </tbody>
     </table>
+    <input type="hidden" name="task" value=""/>
     <input type="hidden" name="boxchecked" value="0" />
+    <?php echo JHtml::_('form.token'); ?>
 </form>
