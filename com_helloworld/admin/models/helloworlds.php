70dc39411e940ab3dac64cc26a84da58d58f3786
Ectotater <36845332+Ectotater@users.noreply.github.com>
2020-06-10 16:44:20 +0800

    Create helloworlds.php

---
+++ com_helloworld/admin/models/helloworlds.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_helloworld
+ * 
+ * @copyright   (C) 2005 - 2018 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+ // No direct access to this file
+defined('_JEXEC') or die('Restricted access');
+
+/**
+ * HelloWorldList Model
+ * 
+ * @since   0.0.1
+ */
+class HelloWorldModelHelloWorlds extends JModelList
+{
+    /**
+     * Method to build an SQL query to load the list data
+     * 
+     * @return      string  An SQL query
+     */
+    protected function getListQuery()
+    {
+        // Initialise variables
+        $db     = JFactory::getDbo();
+        $query  = $db->getQuery(true);
+
+        // Create base select statement
+        $query
+            ->select('*')
+            ->from($db->quoteName('#__helloworld'));
+
+        return $query;
+    }
+}
