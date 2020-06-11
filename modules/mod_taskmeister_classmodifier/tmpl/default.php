fbb71a6eba2cde1ba076f3a661419595e7963625
FremontTeng <fremontteng@gmail.com>
2020-06-11 16:22:57 +0800

    Added adjustable weightages

--- modules/mod_taskmeister_classmodifier/tmpl/default.php
+++ modules/mod_taskmeister_classmodifier/tmpl/default.php
@@ -6,14 +6,24 @@ defined('_JEXEC') or die;
 
 <h3>Class Modifier</h3>
 <span>Likes Weightage: </span><span id="likesValue">100</span>%
-<input oninput="slider_input(this,document.getElementById('likesValue'));" type="range" min="1" max="200" value="100" class="tm_slider" id="likesWeight">
+<input oninput="slider_change(this, 'likesValue');" type="range" min="1" max="200" value="100" class="tm_slider" id="likesWeight">
+<span>Deployment Weightage: </span><span id="deployedValue">100</span>%
+<input oninput="slider_change(this, 'deployedValue');" type="range" min="1" max="200" value="100" class="tm_slider" id="deployedWeight">
+<span>Touched Before Weightage: </span><span id="touchedValue">100</span>%
+<input oninput="slider_change(this, 'touchedValue');" type="range" min="1" max="200" value="100" class="tm_slider" id="touchedWeight">
+
 
 <script>
-var likes_slider = document.getElementById("likesWeight");
-var likes_output = document.getElementById("likesValue");
-likes_output.innerHTML = likes_slider.value;
+var loop_list = ["likes","deployed","touched"];
+function slider_change (slider, input) {
+        var output = document.getElementById(input);
+        output.innerHTML = slider.value;
+    }
 
-slider_input = function(slider, output) {
-  output.innerHTML = slider.value;
+for (i=0;i<loop_list.length;i++){
+    var item = loop_list[i];
+    var slider = document.getElementById(item+"Weight");
+    var output = document.getElementById(item+"Value");
+    output.innerHTML = slider.value;
 }
 </script>
