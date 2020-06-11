dcb2f721c9f6e61ce4b240524b714079ddbd18fe
FremontTeng <fremontteng@gmail.com>
2020-06-11 15:59:09 +0800

    Added custom slider for weightages

--- modules/mod_taskmeister_classmodifier/tmpl/default.php
+++ modules/mod_taskmeister_classmodifier/tmpl/default.php
@@ -5,3 +5,15 @@ defined('_JEXEC') or die;
 ?>
 
 <h3>Class Modifier</h3>
+<span>Likes Weightage: </span><span id="likesValue">100</span>%
+<input oninput="slider_input(this,document.getElementById('likesValue'));" type="range" min="1" max="200" value="100" class="tm_slider" id="likesWeight">
+
+<script>
+var likes_slider = document.getElementById("likesWeight");
+var likes_output = document.getElementById("likesValue");
+likes_output.innerHTML = likes_slider.value;
+
+slider_input = function(slider, output) {
+  output.innerHTML = slider.value;
+}
+</script>
