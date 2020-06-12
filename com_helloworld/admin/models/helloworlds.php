e1908b4a69e330c95c91c98494287689a435b5ce
Ectotater <36845332+Ectotater@users.noreply.github.com>
2020-06-12 15:58:14 +0800

    Implement search

--- com_helloworld/admin/models/helloworlds.php
+++ com_helloworld/admin/models/helloworlds.php
@@ -18,6 +18,27 @@ defined('_JEXEC') or die('Restricted access');
 class HelloWorldModelHelloWorlds extends JModelList
 {
     /**
+     * Constructor
+     * 
+     * @param   array   $config An optional associative array of configuration settings
+     * 
+     * @see     JControlller
+     * @since   1.6
+     */
+    public function __construct($config = array())
+    {
+        if(empty($config['filter_fields']))
+        {
+            $config['filter_fields'] = array(
+                'id',
+                'greeting',
+                'published'
+            );
+        }
+
+        parent::__construct($config);
+    }
+    /**
      * Method to build an SQL query to load the list data
      * 
      * @return      string  An SQL query
@@ -33,6 +54,33 @@ class HelloWorldModelHelloWorlds extends JModelList
             ->select('*')
             ->from($db->quoteName('#__helloworld'));
 
+        // Filter: Like / search
+        $search = $this->getState('filter.search');
+
+        if(!empty($search))
+        {
+            $like = $db->quote('%' . $search . '%');
+            $query->where('greeting LIKE ' . $like);
+         }
+
+        // Filter by published state
+        $published = $this->getState('filter.published');
+
+        if(is_numeric($published))
+        {
+            $query->where('published = ' . (int) $published);
+        }
+        elseif($published === '')
+        {
+            $query->where('(published IN (0, 1))');
+        }
+
+        // Add the list ordering clause
+        $orderCol   = $this->state->get('list.ordering', 'greeting');
+        $orderDirn  = $this->state->get('list.direction', 'asc');
+
+        $query->order($db->escape($orderCol) . ' ' . $db->escape($orderDirn));
+        
         return $query;
     }
 }
