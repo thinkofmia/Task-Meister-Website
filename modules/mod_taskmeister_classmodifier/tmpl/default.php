86578a95a1c8a1ac3ec44e868e5b7a96303a0d1d
FremontTeng <fremontteng@gmail.com>
2020-06-15 16:29:01 +0800

    Added submit button for class modifier mod

--- modules/mod_taskmeister_classmodifier/tmpl/default.php
+++ modules/mod_taskmeister_classmodifier/tmpl/default.php
@@ -5,28 +5,31 @@ defined('_JEXEC') or die;
 ?>
 
 <h3 style="text-align: center;">Class Modifier</h3>
-<!--For Buttons-->
-<span>Likes Weightage: </span><span id="likesValue">100</span>%
-<input oninput="slider_change(this, 'likesValue');" type="range" min="1" max="200" value="100" class="tm_slider" id="likesWeight">
-<span>Deployment Weightage: </span><span id="deployedValue">100</span>%
-<input oninput="slider_change(this, 'deployedValue');" type="range" min="1" max="200" value="100" class="tm_slider" id="deployedWeight">
-<span>Touched Before Weightage: </span><span id="touchedValue">100</span>%
-<input oninput="slider_change(this, 'touchedValue');" type="range" min="1" max="200" value="100" class="tm_slider" id="touchedWeight">
-<!--For Preferences-->
-<span>Preferred Weightage: </span><span id="preferredValue">100</span>%
-<input oninput="slider_change(this, 'preferredValue');" type="range" min="1" max="200" value="100" class="tm_slider" id="preferredWeight">
 
-<span>Don't Prefer Weightage: </span><span id="unpreferredValue">100</span>%
-<input oninput="slider_change(this, 'unpreferredValue');" type="range" min="1" max="200" value="100" class="tm_slider" id="unpreferredWeight">
-<span>May Try Weightage: </span><span id="mayTryValue">100</span>%
-<input oninput="slider_change(this, 'mayTryValue');" type="range" min="1" max="200" value="100" class="tm_slider" id="mayTryWeight">
+<form class="classModifierForm" method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>" >
+  <!--For Buttons-->
+  <span>Likes Weightage: </span><span id="likesValue">100</span>%
+  <input oninput="slider_change(this, 'likesValue');" type="range" min="1" max="200" value="100" class="tm_slider" id="likesWeight">
+  <span>Deployment Weightage: </span><span id="deployedValue">100</span>%
+  <input oninput="slider_change(this, 'deployedValue');" type="range" min="1" max="200" value="100" class="tm_slider" id="deployedWeight">
+  <span>Touched Before Weightage: </span><span id="touchedValue">100</span>%
+  <input oninput="slider_change(this, 'touchedValue');" type="range" min="1" max="200" value="100" class="tm_slider" id="touchedWeight">
+  <!--For Preferences-->
+  <span>Preferred Weightage: </span><span id="preferredValue">100</span>%
+  <input oninput="slider_change(this, 'preferredValue');" type="range" min="1" max="200" value="100" class="tm_slider" id="preferredWeight">
 
-<span title="Toggling this will link your preferences to the class. ">Toggle Preference Linkage</span>
-<label class="switch">
-  <input class="tm_input" type="checkbox">
-  <span class="slider round"></span>
-</label>
+  <span>Don't Prefer Weightage: </span><span id="unpreferredValue">100</span>%
+  <input oninput="slider_change(this, 'unpreferredValue');" type="range" min="1" max="200" value="100" class="tm_slider" id="unpreferredWeight">
+  <span>May Try Weightage: </span><span id="mayTryValue">100</span>%
+  <input oninput="slider_change(this, 'mayTryValue');" type="range" min="1" max="200" value="100" class="tm_slider" id="mayTryWeight">
 
+  <span title="Toggling this will link your preferences to the class. ">Toggle Preference Linkage</span>
+  <label class="switch">
+    <input class="tm_input" type="checkbox">
+    <span class="slider round"></span>
+  </label>
+  <br><input type="submit" name="submitClassModifier" value="Save">
+</form>
 <script>
 var loop_list = ["likes","deployed","touched","preferred","unpreferred","mayTry"];
 function slider_change (slider, input) {
