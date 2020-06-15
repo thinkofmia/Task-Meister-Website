b0ac3b7fd5d93a5d88365eeb097927a84cab07ed
Ectotater <36845332+Ectotater@users.noreply.github.com>
2020-06-15 15:15:49 +0800

    Create controller.php

---
+++ com_taskmeister_review/site/controller.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage    com_taskmeister_review
+ * 
+ * 
+ */
+
+// No direct access to this file
+defined('_JEXEC') or die('Restricted access');
+
+/**
+ * Taskmeister review component Controller
+ * 
+ * @since   0.0.1
+ */
+class TaskmeisterReviewController extends JControllerLegacy
+{
+}
