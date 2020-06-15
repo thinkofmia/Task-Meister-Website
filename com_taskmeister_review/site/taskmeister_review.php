8b4e39d7a9094ec49d76be16f217d613ab73e40e
Ectotater <36845332+Ectotater@users.noreply.github.com>
2020-06-15 15:15:47 +0800

    Create taskmeister_review.php

---
+++ com_taskmeister_review/site/taskmeister_review.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage    com_taskmeister_review
+ * 
+ * 
+ */
+
+// No direct access to this file
+defined('_JEXEC') or die('Restricted access');
+
+// Get an instance of the controller prefixed by TaskmeisterReview
+$controller = JControllerLegacy::getInstance('TaskmeisterReview');
+
+// Perform the Request task
+$input = JFactory::getApplication()->input;
+$controller->execute($input->getCmd('task'));
+
+// Redirect if set by the controller
+$controller->redirect();
