b68ed449d8515d0c65c7ed0ed14e105070fa88f2
Ectotater <36845332+Ectotater@users.noreply.github.com>
2020-06-15 15:15:43 +0800

    Create view.html.php

---
+++ com_taskmeister_review/site/views/taskmeisterreview/view.html.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage    com_taskmeister_review
+ * 
+ * 
+ */
+
+// No direct access to this file
+defined('_JEXEC') or die('Restricted access');
+
+/**
+ * HTML View class for the TaskmeisterReview Component
+ * 
+ * @since   0.0.1
+ */
+class TaskmeisterReviewViewTaskmeisterReview extends JViewLegacy
+{
+    /**
+     * Display the Taskmeister Review view
+     * 
+     * @param   string  $tpl    The name of the template file to parse; automatically searches through the template paths
+     * 
+     * @return  void
+     */
+    function display($tpl = null)
+    {
+        // Assign data to the view
+        $this->smg = 'Taskmeister review';
+
+        // Display the view
+        parent::display($tpl);
+    }
+}
