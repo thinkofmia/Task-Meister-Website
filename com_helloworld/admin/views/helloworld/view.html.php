24fd02109c240aa10ca2dceaab152ba8be7b9814
Ectotater <36845332+Ectotater@users.noreply.github.com>
2020-06-15 12:47:58 +0800

    Update view.html.php

--- com_helloworld/admin/views/helloworld/view.html.php
+++ com_helloworld/admin/views/helloworld/view.html.php
@@ -31,6 +31,7 @@ class HelloWorldViewHelloWorld extends JViewLegacy
         // Get the data
         $this->form = $this->get('form');
         $this->item = $this->get('Item');
+        $this->script = $this->get('Script');
 
         // Check for errors
         if(count($errors = $this->get('Errors')))
@@ -89,10 +90,17 @@ class HelloWorldViewHelloWorld extends JViewLegacy
      */
     protected function setDocument()
     {
+        JHtml::_('behavior.framework');
+        JHtml::_('behavior.formvalidator');
+
         $isNew = ($this->item->id < 1);
         $document = JFactory::getDocument();
         $document->setTitle($isNew ? JText::_('COM_HELLOWORLD_HELLOWORLD_CREATING') :
             JText::_('COM_HELLOWORLD_HELLOWORLD_EDTING'));
+        $document->addScript(JURI::root() . $this->script);
+        $document->addScript(JURI::root() . "/administrator/components/com_helloworld"
+                                          . "/views/helloworld/submitbutton.js");
+        JText::script('COM_HELLOWORLD_HELLOWORLD_ERROR_UNACCEPTABLE');
     }
 }
 
