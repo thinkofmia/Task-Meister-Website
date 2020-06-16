8976a0ab8f7d9329eb0fab890ba95991fa16055a
Ectotater <36845332+Ectotater@users.noreply.github.com>
2020-06-16 14:42:08 +0800

    Update taskmeisterreview.php

--- com_taskmeister_review/site/models/taskmeisterreview.php
+++ com_taskmeister_review/site/models/taskmeisterreview.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package     Joomla.Administrator
- * @subpackage  com_helloworld
+ * @subpackage  com_taskmeister_review
  * 
  * @copyright   (C) 2005 - 2018 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
@@ -18,31 +18,56 @@ defined('_JEXEC') or die('Restricted access');
 class TaskmeisterReviewModelTaskmeisterReview extends JModelItem
 {
     /**
-     * @var string  message;
+     * @var array   reviews
      */
-    protected $message;
+    protected $reviews;
 
     /**
-     * Get the message
+     * Method to get a table object, load it if necessary
      * 
-     * @return  string  The message to be displayed to the user
+     * @param   string  $type   The table name (optional)
+     * @param   string  $prefix The class prefix (optional)
+     * @param   array   $config Configuraton array for model (optional)
+     * 
+     * @return  JTable  A JTable object
+     * 
+     * @since 1.6
      */
-    public function getMsg()
+    public function getTable($type = 'TaskmeisterReview', $prefix = 'TaskmeisterReviewTable', $config = array())
     {
-       $input = JFactory::getApplication()->input;
-       $id = $input->get('id', 1, 'int');
+        return JTable::getInstance($type, $prefix, $config);
+    }
 
-       switch($id)
-       {
-           case 2:
-            $this->message = 'Review 1 Good';
-           break;
-           default:
-           case 1:
-            $this->message = 'Review 2 Terrible';
-           break;
-       }
+    /**
+     * Get the review
+     * 
+     * @param   integer $id Review id
+     * 
+     * @return  string      Fetched string from Table for relevant id
+     */
+    public function getMsg($id = 1)
+    {
+        if(!is_array($this->reviews))
+        {
+            $this->reviews = array();
+        }
 
-        return $this->message;
+        if(!isset($this->reviews[$id]))
+        {
+            // Request the selected id
+            $input = JFactory::getApplication()->input;
+            $id = $input->get('id', 1, 'int');
+
+            // get a TableTaskmeisterReview instance
+            $table = $this->getTable();
+
+            // Load the message
+            $table->load($id);
+
+            // Assign the review
+            $this->reviews[$id] = $table->ease;
+        }
+
+        return $this->reviews[$id];
     }
 }
