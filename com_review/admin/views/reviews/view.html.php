d22cbcf3015d24fcbc9cf553971d8de5ff5f8228
Ectotater <36845332+Ectotater@users.noreply.github.com>
2020-06-17 18:11:55 +0800

    Add sorting and filtering

--- com_review/admin/views/reviews/view.html.php
+++ com_review/admin/views/reviews/view.html.php
@@ -26,9 +26,18 @@ class ReviewViewReviews extends JViewLegacy
      */
     function display($tpl = null)
     {
+        // Get application
+        $app = JFactory::getApplication();
+        $context = "review.list.admin.review";
+
         // Get data from the model
-        $this->items        = $this->get('Items');
-        $this->pagination   = $this->get('Pagination');
+        $this->items            = $this->get('Items');
+        $this->pagination       = $this->get('Pagination');
+        $this->state            = $this->get('State');
+        $this->filter_order     = $app->getUserStateFromRequest($context.'filter_order', 'filter_order', 'AID', 'cmd');
+        $this->filter_order_Dir = $app->getUserStateFromRequest($context.'filter_order_Dir', 'filter_order_Dir', 'asc', 'cmd');
+        $this->filterForm       = $this->get('FilterForm');
+        $this->activeFilters    = $this->get('ActiveFilters');
 
         // Check for errors
         if(count($errors = $this->get('Errors')))
@@ -38,11 +47,14 @@ class ReviewViewReviews extends JViewLegacy
             return false;
         }
 
-        // Set the toolbar
+        // Set the toolbar and number of found items
         $this->addToolBar();
 
         // Display the template
         parent::display($tpl);
+
+        // Set the document
+        $this->setDocument();
     }
 
     /**
@@ -54,9 +66,29 @@ class ReviewViewReviews extends JViewLegacy
      */
     protected function addToolBar()
     {
-        JToolbarHelper::title(JText::_('COM_REVIEW_MANAGER_REVIEWS'));
+        $title = JText::_('COM_REVIEW_MANAGER_REVIEWS');
+
+        if($this->pagination->total)
+        {
+            $title .= " <span style='font-size: 0.5cm; vertical-align: middle;'>(" . $this->pagination->total . ")</span>";
+        }
+
+
+        JToolbarHelper::title($title, 'review');
         JToolbarHelper::addNew('review.add');
         JToolbarHelper::editList('review.edit');
         JToolbarHelper::deleteList('', 'reviews.delete');
+        JToolbarHelper::publishList('reviews.publish');
+        JToolbarHelper::unpublishList('reviews.unpublish');
+    }
+    /**
+     * Method to set up the document properties
+     * 
+     * @return  void
+     */
+    protected function setDocument()
+    {
+        $document = JFactory::getDocument();
+        $document->setTitle(JText::_('COM_REVIEW_ADMINISTRATION'));
     }
 }
