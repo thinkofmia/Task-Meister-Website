9620b928407743300166f57f80a2fff9fd233512
Ectotater <36845332+Ectotater@users.noreply.github.com>
2020-06-17 18:09:45 +0800

    Copied placeholder files

---
+++ mod_review/helper.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package Joomla.Site
+ * @subpackage mod_taskmeister_review
+ * @license GNU/GPL, see LICENSE.php
+ * mod_helloworld is free software. This version may have been modified pursuant
+ * to the GNU General Public License, and as distributed it includes or
+ * is derivative of works licensed under the GNU General Public License or
+ * other free or open source software licenses.
+**/
+// no direct access
+defined('_JEXEC') or die;
+?>
+
+<p>Hello from helper.php of mod_taskmeister_review</p>
