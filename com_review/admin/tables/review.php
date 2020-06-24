03fb2d417a91e9d8c62487427c044ebfcc18388d
Ectotater <36845332+Ectotater@users.noreply.github.com>
2020-06-24 13:16:55 +0800

    Update review.php
    Copied __construct() function from Joomla source, modified to unset() generated columns via hardcoded values

--- com_review/admin/tables/review.php
+++ com_review/admin/tables/review.php
@@ -22,8 +22,71 @@ class ReviewTableReview extends JTable
      * 
      * @param   JDatabaseDriver &$db    A database connector object
      */
-    function __construct(&$db)
+    function __construct(&$db, $table = 'reviews', $key = 'id')
     {
-        parent::__construct('reviews', 'id', $db);
+        // Set internal variables.
+        $this->_tbl = $table;
+    
+        // Set the key to be an array.
+        if (is_string($key))
+        {
+            $key = array($key);
+        }
+        elseif (is_object($key))
+        {
+            $key = (array) $key;
+        }
+
+        $this->_tbl_keys = $key;
+
+        if (count($key) == 1)
+        {
+            $this->_autoincrement = true;
+        }
+        else
+        {
+            $this->_autoincrement = false;
+        }
+
+        // Set the singular table key for backwards compatibility.
+        $this->_tbl_key = $this->getKeyName();
+
+        $this->_db = $db;
+
+        // Initialise the table properties.
+        $fields = $this->getFields();
+
+        // ignore generated columns
+        unset($fields['auid']);
+        unset($fields['overall_rating']);
+
+        if ($fields)
+        {
+            foreach ($fields as $name => $v)
+            {
+                // Add the field if it is not already present.
+                if (!property_exists($this, $name))
+                {
+                    $this->$name = null;
+                }
+            }
+        }
+
+        // If we are tracking assets, make sure an access field exists and initially set the default.
+        if (property_exists($this, 'asset_id'))
+        {
+            $this->_trackAssets = true;
+        }
+
+        // If the access property exists, set the default.
+        if (property_exists($this, 'access'))
+        {
+            $this->access = (int) \JFactory::getConfig()->get('access');
+        }
+
+        // Implement \JObservableInterface:
+        // Create observer updater and attaches all observers interested by $this class:
+        $this->_observers = new \JObserverUpdater($this);
+        \JObserverMapper::attachAllObservers($this);
     }
 }
