accab983202fe563ff9f3b58042f36f656272176
FremontTeng <fremontteng@gmail.com>
2020-06-26 18:11:39 +0800

    Fixed css and display of recent recommendations mod

--- modules/mod_taskmeister_recentrecommendation/mod_taskmeister_recentrecommendation.php
+++ modules/mod_taskmeister_recentrecommendation/mod_taskmeister_recentrecommendation.php
@@ -21,7 +21,7 @@ require_once dirname(__FILE__) . '/helper.php';//used because our helper functio
 $displayHeader = modRecentRecommend::getHeader($params);//set variable of header using helper class
 $displayText = modRecentRecommend::getText($params);//set variable of text using helper class
 $size = $params->get('size');
-require JModuleHelper::getLayoutPath('mod_taskmeister_recentrecommendation');//call out default.php display
+
 
 //Database code
 use Joomla\CMS\Factory;
@@ -36,42 +36,8 @@ $db->setQuery($query);
 $results = $db->loadAssocList();//Save results of external article database query
 
 if ($results){//If inside article table
-
-//Display table, table header and css style
-echo "
-<style>
-    table, tr, th, td {
-    border: 2px solid black;
-    }
-</style>
-<table style='border: 2px solid black; border-collapse: collapse;'>
-<tr>
-    <th>Date</th>
-    <th>Action</th>
-</tr>";
-
-$counter = 0;//Initialize counter
-foreach ($results as $row) {
-    if ($counter<$size){
-/*
-    Display rows based on main database result
-        $row['date'] refers to the record date
-        $row['es_uid'] refers to the User ID
-        $row['es_aid'] refers to the article ID
-        $row['es_action'] refers to the action done
-*/
-    $user = JFactory::getUser(intval($row['es_uid']));
-    $username = $user->name;
-    $article =& JTable::getInstance("content");
-    $article->load(intval($row['es_aid']));
-    $articleTitle= $article->get("title");
-
-    echo "<tr><td>" . $row['es_date'] . "</td>";
-    echo "<td> User " . $username ." ". $row['es_action']. " article " .$articleTitle. "</td></tr>";
-    $counter = $counter + 1;
-    }
-}
-echo "</table>";
+    $counter = 0;//Initialize counter
+    require JModuleHelper::getLayoutPath('mod_taskmeister_recentrecommendation');//call out default.php display
 }
 
 else{//If no recent recommendationa actions
--- modules/mod_taskmeister_recentrecommendation/tmpl/default.php
+++ modules/mod_taskmeister_recentrecommendation/tmpl/default.php
@@ -5,3 +5,31 @@ defined('_JEXEC') or die;
 ?>
 
 Recent Recommendations <!--Header for the tables-->
+<table class="yellowtable">
+<tr>
+    <th>Date</th>
+    <th>Action</th>
+</tr>
+<?php foreach ($results as $row) : ?>
+    <?php if ($counter<$size) : ?>
+    <?php /*
+    Display rows based on main database result
+        $row['date'] refers to the record date
+        $row['es_uid'] refers to the User ID
+        $row['es_aid'] refers to the article ID
+        $row['es_action'] refers to the action done
+    */
+    $user = JFactory::getUser(intval($row['es_uid']));
+    $username = $user->name;
+    $article =& JTable::getInstance("content");
+    $article->load(intval($row['es_aid']));
+    $articleTitle= $article->get("title");
+    ?>
+    <tr>
+        <td><?php echo $row['es_date']; ?></td>
+        <td><?php echo "User ".$username." ".$row['es_action']. " article " .$articleTitle;?></td>
+    </tr>
+    <?php $counter = $counter + 1; ?>
+    <?php endif; ?>
+<?php endforeach; ?>
+</table>
