b8a659674aa21625dd8d053b1201b38614715820
FremontTeng <fremontteng@gmail.com>
2020-06-26 16:33:21 +0800

    Updated base mod for recent recommendations

---
+++ modules/mod_taskmeister_recentrecommendation/helper.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Helper class for Recent Recommendations module
+ * 
+ * @package    Joomla.Tutorials
+ * @subpackage Modules
+ * @link http://docs.joomla.org/J3.x:Creating_a_simple_module/Developing_a_Basic_Module
+ * @license        GNU/GPL, see LICENSE.php
+ * mod_helloworld is free software. This version may have been modified pursuant
+ * to the GNU General Public License, and as distributed it includes or
+ * is derivative of works licensed under the GNU General Public License or
+ * other free or open source software licenses.
+ */
+class modRecentRecommend
+{
+    /**
+     * Retrieves the hello message
+     *
+     * @param   array  $params An object containing the module parameters
+     *
+     * @access public
+     */    
+    public static function getText($params) //Get text input from the Joomla's interface
+    {
+        return $params->get('customtext');
+    }
+    public static function getHeader($params)//Get header input from the Joomla's interface
+    {
+        return $params->get('customheader');
+    }
+}
---
+++ modules/mod_taskmeister_recentrecommendation/mod_taskmeister_recentrecommendation.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Recent Recommendations Module Entry Point
+ * 
+ * @package    Joomla.Tutorials
+ * @subpackage Modules
+ * @license    GNU/GPL, see LICENSE.php
+ * @link       http://docs.joomla.org/J3.x:Creating_a_simple_module/Developing_a_Basic_Module
+ * mod_helloworld is free software. This version may have been modified pursuant
+ * to the GNU General Public License, and as distributed it includes or
+ * is derivative of works licensed under the GNU General Public License or
+ * other free or open source software licenses.
+ */
+
+// No direct access
+defined('_JEXEC') or die; // ensures that this file is being invoked from the Joomla! application. This is necessary to prevent variable injection and other security vulnerabilities. 
+// Include the syndicate functions only once
+require_once dirname(__FILE__) . '/helper.php';//used because our helper functions are defined within a class, and we only want the class defined once. 
+
+
+$displayHeader = modRecentRecommend::getHeader($params);//set variable of header using helper class
+$displayText = modRecentRecommend::getText($params);//set variable of text using helper class
+require JModuleHelper::getLayoutPath('mod_taskmeister_recentrecommendation');//call out default.php display
+
+//Database code
+use Joomla\CMS\Factory;
+$db = Factory::getDbo();//Get database
+
+//Querying SQL database for recent recommendations
+$query = $db->getQuery(true);
+$query->select($db->quoteName(array('*')))//Sets which columns of database
+    ->from($db->quoteName('#__customtables_table_recommendationstats'))
+    ->order($db->quoteName('id') . ' DESC');//Sets which database
+$db->setQuery($query);
+$results = $db->loadAssocList();//Save results of external article database query
+
+if ($results){//If inside article table
+
+//Display table, table header and css style
+echo "
+<style>
+    table, tr, th, td {
+    border: 2px solid black;
+    }
+</style>
+<table style='border: 2px solid black; border-collapse: collapse;'>
+<tr>
+    <th>Date</th>
+    <th>Action</th>
+</tr>";
+
+foreach ($results as $row) {
+/*
+    Display rows based on main database result
+        $row['id'] refers to the recommendation id
+        $row['title'] refers to the article title
+        $row['catid'] refers to the article category
+        $row['hits'] refers to the article hits
+        $row['featured'] refers whether the article is featured or not
+*/
+    echo "<tr><td>" . $row['es_date'] . "</td>" . 
+    "<td>" . $row['es_uid']." does ". $row['es_action']. " on " .$row['es_aid']. "</td></tr></table>";
+}
+
+}
+
+else{//If no recent recommendationa actions
+    echo "No recent actions. ";
+
+}
