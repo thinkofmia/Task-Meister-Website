ff3a1c5146af3cb7ecc5af62e94acea1d33c5f1a
FremontTeng <fremontteng@gmail.com>
2020-07-06 17:35:35 +0800

    Added basic chart visualization

--- modules/mod_taskmeister_yourclassstats/tmpl/default.php
+++ modules/mod_taskmeister_yourclassstats/tmpl/default.php
@@ -10,33 +10,33 @@ defined('_JEXEC') or die;
 <h3>Your Class Stats</h3>
 
 <!--Code for Bar Chart-->
-<div class="chart-container" style="position: relative; width:80vw">
+<div class="chart-container" style="position: relative;">
     <canvas id="barChart" width="400" height="400"></canvas>
 </div>
 <script>
+//Set Chart Size Responsively
+var chart = document.getElementById('barChart');
+chart.parentNode.style.width = '90vw';
+//Display chart
 var ctx = document.getElementById('barChart').getContext('2d');
 var myChart = new Chart(ctx, {
     type: 'bar',
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: ['<?php echo implode("', '",array_keys($fullPreferencesScore)); ?>'],
         datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
+            label: 'Preferences Score',
+            data: [<?php echo implode(', ',$fullPreferencesScore); ?>],
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
+                <?php foreach ($fullPreferencesScore as $key => $value) : ?>
+                    "<?php echo 'rgba('.rand(50, 255).', '.rand(50, 255).', '.rand(50, 255).', 0.2) '; ?>",
+                <?php endforeach; ?>
+                "<?php echo 'rgba('.rand(50, 255).', '.rand(50, 255).', '.rand(50, 255).', 0.2) '; ?>"
             ],
             borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
+                <?php foreach ($fullPreferencesScore as $key => $value) : ?>
+                    "<?php echo 'rgba('.rand(50, 255).', '.rand(50, 255).', '.rand(50, 255).', 0.2)'; ?>",
+                <?php endforeach; ?>
+                "<?php echo 'rgba('.rand(50, 255).', '.rand(50, 255).', '.rand(50, 255).', 0.2) '; ?>"
             ],
             borderWidth: 1
         }]
