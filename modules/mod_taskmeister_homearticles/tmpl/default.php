afda2d7f1a29faebe6354f141752ca67a6cca3b3
FremontTeng <fremontteng@gmail.com>
2020-07-17 10:08:13 +0800

    Added checks if articles exists in category

--- modules/mod_taskmeister_homearticles/tmpl/default.php
+++ modules/mod_taskmeister_homearticles/tmpl/default.php
@@ -17,6 +17,7 @@ defined('_JEXEC') or die;
 <!--Display-->
 <?php foreach ($recommendedContents as $tag => $contents) : ?>
   <h3>Because you liked <?php echo $tag; ?>...</h3>
+  <?php if ($contents || sizeof($contents)>0) : ?>
   <ul class="scrollbarnews<?php echo $moduleclass_sfx; ?> mod-list">
   <div class="articlesRow">
     <!-- Arrow button to scroll left-->
@@ -32,37 +33,37 @@ defined('_JEXEC') or die;
     <div class="recommendedArticles dragscroll" id= "recommendation">
       <!--For loop for the items in the list-->
       <?php $count = 0; //For counting the number of articles in list?>
-      <?php foreach ($contents as $key => $value) : ?>
-        <?php $count+=1; ?>
-        <!--Url link to the article page-->
-        <a title="<?echo $value[0]; ?>" href="?option=com_content&view=article&id=<?php echo $key; ?>" itemprop="url">
-          <?php if ($displayMode=="display_ranking") : ?>
-            <div class="rankedArticle" title="Match: <?php echo json_encode($value[2]); ?>%">
-              <?php if ($count>9) : ?>
-                <h4><?php echo $count; ?></h4>
-              <?php else : ?>
-                <h2><?php echo $count;?></h2>
-              <?php endif; ?>
-              <!--Image of the article-->
-              <img onerror="this.src='/taskmeisterx/modules/mod_taskmeister_recommendarticles/img/default.jpg';" src="<?php echo json_decode($value[1])->image_intro; ?>" width="100%" height="100%" />
-              <!--Text found on the article-->
-              <p>
-              <?php if (strlen($value[0])>50) echo substr($value[0], 0, 50)."...";
-                else echo $value[0]; ?></p>
-            </div>
-          <?php else : ?>
-            <div class="article">
-              <!--Image of the article-->
-              <img onerror="this.src='/taskmeisterx/modules/mod_taskmeister_recommendarticles/img/default.jpg';" src="<?php echo json_decode($value[1])->image_intro; ?>" width="100%" height="100%" />
-              <!--Text found on the article-->
-              <p class="articleSimilarity">Match: <?php echo json_encode($value[2]); ?>%</p>
-              <p>
-              <?php if (strlen($value[0])>50) echo substr($value[0], 0, 50)."...";
-                else echo $value[0]; ?></p>
-            </div>
-          <?php endif; ?>
-        </a>
-      <?php endforeach; ?>
+        <?php foreach ($contents as $key => $value) : ?>
+          <?php $count+=1; ?>
+          <!--Url link to the article page-->
+          <a title="<?echo $value[0]; ?>" href="?option=com_content&view=article&id=<?php echo $key; ?>" itemprop="url">
+            <?php if ($displayMode=="display_ranking") : ?>
+              <div class="rankedArticle" title="Match: <?php echo json_encode($value[2]); ?>%">
+                <?php if ($count>9) : ?>
+                  <h4><?php echo $count; ?></h4>
+                <?php else : ?>
+                  <h2><?php echo $count;?></h2>
+                <?php endif; ?>
+                <!--Image of the article-->
+                <img onerror="this.src='/taskmeisterx/modules/mod_taskmeister_recommendarticles/img/default.jpg';" src="<?php echo json_decode($value[1])->image_intro; ?>" width="100%" height="100%" />
+                <!--Text found on the article-->
+                <p>
+                <?php if (strlen($value[0])>50) echo substr($value[0], 0, 50)."...";
+                  else echo $value[0]; ?></p>
+              </div>
+            <?php else : ?>
+              <div class="article">
+                <!--Image of the article-->
+                <img onerror="this.src='/taskmeisterx/modules/mod_taskmeister_recommendarticles/img/default.jpg';" src="<?php echo json_decode($value[1])->image_intro; ?>" width="100%" height="100%" />
+                <!--Text found on the article-->
+                <p class="articleSimilarity">Match: <?php echo json_encode($value[2]); ?>%</p>
+                <p>
+                <?php if (strlen($value[0])>50) echo substr($value[0], 0, 50)."...";
+                  else echo $value[0]; ?></p>
+              </div>
+            <?php endif; ?>
+          </a>
+        <?php endforeach; ?>
     </div>
     <!-- Arrow button to scroll right-->
     <i class="arrowRight" onclick="var left = this.closest('div').querySelector('.recommendedArticles').scrollLeft; this.closest('div').querySelector('.recommendedArticles').scrollTo({
@@ -75,6 +76,9 @@ defined('_JEXEC') or die;
           });"></i>
   </div>
   </ul>
+  <?php else : ?>
+          No relevant articles found.
+  <?php endif; ?>
   <?php endforeach; ?>
 
   <script type="text/javascript" src="https://cdn.rawgit.com/asvd/dragscroll/master/dragscroll.js"></script>
