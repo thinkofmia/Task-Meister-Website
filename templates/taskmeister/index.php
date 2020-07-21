79fc793317e530e9bccd2e9087584518f51faa6c
FremontTeng <fremontteng@gmail.com>
2020-07-21 15:08:40 +0800

    Reordered the position of the js

--- templates/taskmeister/index.php
+++ templates/taskmeister/index.php
@@ -35,8 +35,6 @@ $doc->addScript($this->baseurl . '/templates/' . $this->template . '/js/keen-sli
 <!-- Contain the information about the document-->
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<!-- Puts the correct header information in (eg. page title, meta information, JavaScript)-->
-<jdoc:include type="head" />
 <!--Bootstrap 4-->
 <!-- CSS only -->
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
@@ -44,6 +42,9 @@ $doc->addScript($this->baseurl . '/templates/' . $this->template . '/js/keen-sli
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+<!-- Puts the correct header information in (eg. page title, meta information, JavaScript)-->
+<jdoc:include type="head" />
+
 <!-- Creates links to two system style sheets and to our style sheet-->
 <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/system/css/system.css" type="text/css" />
 <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/system/css/general.css" type="text/css" />
