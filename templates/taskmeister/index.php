3e4ffb97e7978467728cead747cf63560ae27759
FremontTeng <fremontteng@gmail.com>
2020-07-24 17:29:42 +0800

    Worked on taskmeister feed.

--- templates/taskmeister/index.php
+++ templates/taskmeister/index.php
@@ -42,8 +42,6 @@ $doc->addScript($this->baseurl . '/templates/' . $this->template . '/js/keen-sli
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
-<!-- Puts the correct header information in (eg. page title, meta information, JavaScript)-->
-<jdoc:include type="head" />
 
 <!-- Creates links to two system style sheets and to our style sheet-->
 <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/system/css/system.css" type="text/css" />
@@ -51,6 +49,9 @@ $doc->addScript($this->baseurl . '/templates/' . $this->template . '/js/keen-sli
 <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/css/template.css" type="text/css" />
 <link href="https://fonts.googleapis.com/css2?family=BioRhyme&display=swap" rel="stylesheet">
 <script src="https://www.google.com/jsapi"></script>
+
+<!-- Puts the correct header information in (eg. page title, meta information, JavaScript)-->
+<jdoc:include type="head" />
 <!--Importing CSS changes from old protostartina template-->
 <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/css/old.css" type="text/css" />
 <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/css/keen-slider.min.css" type="text/css" />
