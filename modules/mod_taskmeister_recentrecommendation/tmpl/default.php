fdcc7f44f9ef52738d07359c2d36a801b2899c6a
FremontTeng <fremontteng@gmail.com>
2020-08-03 10:43:42 +0800

    Updated documentation for the recentrecommendation module

--- modules/mod_taskmeister_recentrecommendation/tmpl/default.php
+++ modules/mod_taskmeister_recentrecommendation/tmpl/default.php
@@ -4,13 +4,18 @@ defined('_JEXEC') or die;
 //Displays module output
 ?>
 
-Recent Recommendations <!--Header for the tables-->
+<!--Header for the tables-->
+Recent Recommendations 
+<!--Table-->
 <table class="yellowtable">
 <tr>
+    <!--Headers-->
     <th>Date</th>
     <th>Action</th>
 </tr>
+<!--Loop for each action in the $results-->
 <?php foreach ($results as $row) : ?>
+    <!--If yet to exceed the max number of actions to display-->
     <?php if ($counter<$size) : ?>
     <?php /*
     Display rows based on main database result
@@ -19,17 +24,19 @@ Recent Recommendations <!--Header for the tables-->
         $row['es_aid'] refers to the article ID
         $row['es_action'] refers to the action done
     */
-    $user = JFactory::getUser(intval($row['es_uid']));
-    $username = $user->name;
-    $article =& JTable::getInstance("content");
-    $article->load(intval($row['es_aid']));
-    $articleTitle= $article->get("title");
+    $user = JFactory::getUser(intval($row['es_uid']));//Gets user variable
+    $username = $user->name;//Gets user name
+    $article =& JTable::getInstance("content");//Sets content variable
+    $article->load(intval($row['es_aid']));//Gets article by their id
+    $articleTitle= $article->get("title");//Get article title
     ?>
     <tr>
+        <!--Display the date of action-->
         <td><?php echo $row['es_date']; ?></td>
+        <!--Display the action-->
         <td><?php echo "User ".$username." ".$row['es_action']. " article " .$articleTitle;?></td>
     </tr>
-    <?php $counter = $counter + 1; ?>
+    <?php $counter = $counter + 1; //Increment counter by 1 ?>
     <?php endif; ?>
 <?php endforeach; ?>
 </table>
